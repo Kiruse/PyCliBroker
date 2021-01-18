bcc9e36f1ac5439f540d9db56b30f5d51465396b
Kiruse <neurokiruse@outlook.com>
2021-01-18 02:13:17 +0100

    renamed manual test in order to exclude it from pytest suite

--- test/manual.py
+++ test/manual.py
@@ -1,6 +1,10 @@
 """Simple manual test to verify proper functioning with std-streams. Complimentary to pytest unit tests.
 Copyright (c) Kiruse 2020. See license in LICENSE."""
+from os import path
 import asyncio
+import sys
+
+sys.path.append(path.dirname(path.dirname(__file__)))
 import clibroker as cli
 
 async def main():
@@ -10,15 +14,15 @@ async def main():
     await asyncio.wait((t1, t2))
 
 async def async1():
-    async with cli.session(autoflush=True) as sess:
-        await sess.write("Say something, I'm giving up on you... ")
-        response = await sess.readline()
-        if len(response) > 0:
-            await sess.writeline(f'Too late now. >:c')
-
-async def async2():
     await asyncio.sleep(0.1)
     pw = await cli.password('Secret: ')
     await cli.writeline(f'Your secret is {pw}')
+
+async def async2():
+    with await cli.session(autoflush=True) as sess:
+        await sess.write("Hello! Are you there? Say something! ")
+        response = await sess.readline()
+        if len(response) > 0:
+            await sess.writeline(f'Too late now. >:c')
 
 asyncio.run(main())
